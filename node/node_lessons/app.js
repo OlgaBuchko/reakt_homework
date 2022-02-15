abc3b6c6f80e8771ff46f9be793be55b6ebdab1b
OlgaBuchko <lyolyabo92@gmail.com>
2022-02-15 20:04:34 +0200

    e

--- node/node_lessons/app.js
+++ node/node_lessons/app.js
@@ -19,17 +19,17 @@ fs.mkdir(path.join(__dirname,'main', 'inPerson'),{recursive:true},err => {
 
 let onlineUsers=[
     {name: 'Oleg',
-    age:32,
-    city:'Lviv'},
+        age:32,
+        city:'Lviv'},
     {name:'Ira',
-    age:21,
-    city:'Ternopil'}
+        age:21,
+        city:'Ternopil'}
 ];
 
 let inPersonUsers=[
     {name:'Petro',
-    age:43,
-    city:'Kyiv'},
+        age:43,
+        city:'Kyiv'},
     {
         name:'Marina',
         age:22,
@@ -38,25 +38,25 @@ let inPersonUsers=[
 ]
 const onlineFile = (arrInOnlineFile)=>{
     arrInOnlineFile.map(user=>{
-    fs.writeFile(path.join(__dirname,'main','online','online.txt'),`\n NAME: ${user.name} \n AGE: ${user.age} \n CITY: ${user.city} \n \n `,{flag: 'a'},(err)=> {
-        if (err) {
-            console.log(err);
-            throw err;
-        }
-})
-})}
+        fs.writeFile(path.join(__dirname,'main','online','online.txt'),`\n NAME: ${user.name} \n AGE: ${user.age} \n CITY: ${user.city} \n \n `,{flag: 'a'},(err)=> {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+        })
+    })}
 
 
 const inPersonFile = (arrInPersonFile)=>{
     arrInPersonFile.map(user=>{
-    fs.writeFile(path.join(__dirname,'main','inPerson','inPerson.txt'),`\n NAME: ${user.name} \n AGE: ${user.age} \n CITY: ${user.city} \n \n `,{flag: 'a'},(err)=> {
-        if (err) {
-            console.log(err);
-            throw err;
-        }
-    })
+        fs.writeFile(path.join(__dirname,'main','inPerson','inPerson.txt'),`\n NAME: ${user.name} \n AGE: ${user.age} \n CITY: ${user.city} \n \n `,{flag: 'a'},(err)=> {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+        })
 
-})}
+    })}
 inPersonFile(inPersonUsers);
 onlineFile(onlineUsers);
 
@@ -76,6 +76,6 @@ const changePlaces = function (arrInOnlineFile,arrInPersonFile) {
         }
     })
     onlineFile(arrInOnlineFile);
-inPersonFile(arrInPersonFile)
+    inPersonFile(arrInPersonFile)
 }
 changePlaces(inPersonUsers,onlineUsers)
